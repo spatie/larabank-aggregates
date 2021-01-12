1b3e0068c9011a938e6474e8460d698a8cf58f15
Laravel Shift <shift@laravelshift.com>
2021-01-12 14:49:34 +0000

    Adopt Laravel coding style
    Shift automatically applies the Laravel coding style - which uses the PSR-2 coding style as a base with some minor additions.
    
    You may customize the adopted coding style by adding your own [PHP CS Fixer][1] `.php_cs` config file to your project root. Feel free to use [Shift's Laravel ruleset][2] to help you get started.
    
    [1]: https://github.com/FriendsOfPHP/PHP-CS-Fixer
    [2]: https://gist.github.com/laravel-shift/cab527923ed2a109dda047b97d53c200

--- app/Domain/Account/Reactors/OfferLoanReactor.php
+++ app/Domain/Account/Reactors/OfferLoanReactor.php
@@ -7,8 +7,6 @@ use App\Domain\Account\Events\MoreMoneyNeeded;
 use App\Mail\LoanProposalMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Mail;
-use Spatie\EventSourcing\EventHandlers\EventHandler;
-use Spatie\EventSourcing\EventHandlers\HandlesEvents;
 use Spatie\EventSourcing\EventHandlers\Reactors\Reactor;
 
 final class OfferLoanReactor extends Reactor implements ShouldQueue

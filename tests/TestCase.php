4079e84af8031fea35b54b1194a362c833717a87
phamthao <phamthao03596@gmail.com>
2021-08-13 22:47:05 +0700

    add test AccountAggregateRoot

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,26 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Throwable;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+    use RefreshDatabase;
+
+    protected function assertExceptionThrown(callable $callable, string $expectedExceptionClass): void
+    {
+        try {
+            $callable();
+
+            $this->assertTrue(false, "Expected exception `{$expectedExceptionClass}` was not thrown.");
+        } catch (Throwable $exception) {
+            if (! $exception instanceof $expectedExceptionClass) {
+                throw $exception;
+            }
+            $this->assertInstanceOf($expectedExceptionClass, $exception);
+        }
+    }
 }

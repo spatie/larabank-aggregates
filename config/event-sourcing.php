b01d8021680821ecb9710a0c7c8b0ed8fa97ed4f
phamthao <phamthao03596@gmail.com>
2021-08-13 19:55:40 +0700

    update spatie/laravel-event-sourcing to v5

--- config/event-sourcing.php
+++ config/event-sourcing.php
@@ -11,6 +11,12 @@ return [
     ],
 
     /*
+     * This directory will be used as the base path when scanning
+     * for projectors and reactors.
+     */
+    'auto_discover_base_path' => base_path(),
+
+    /*
      * Projectors are classes that build up projections. You can create them by performing
      * `php artisan event-sourcing:create-projector`. When not using auto-discovery,
      * Projectors can be registered in this array or a service provider.
@@ -44,30 +50,37 @@ return [
     /*
      * This class is responsible for storing events in the EloquentStoredEventRepository.
      * To add extra behaviour you can change this to a class of your own. It should
-     * extend the \Spatie\EventSourcing\Models\EloquentStoredEvent model.
+     * extend the \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent model.
      */
-    'stored_event_model' => \Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent::class,
+    'stored_event_model' => Spatie\EventSourcing\StoredEvents\Models\EloquentStoredEvent::class,
 
     /*
      * This class is responsible for storing events. To add extra behaviour you
      * can change this to a class of your own. The only restriction is that
-     * it should implement \Spatie\EventSourcing\EloquentStoredEventRepository.
+     * it should implement \Spatie\EventSourcing\StoredEvents\Repositories\EloquentStoredEventRepository.
      */
-    'stored_event_repository' => \Spatie\EventSourcing\StoredEvents\Repositories\EloquentStoredEventRepository::class,
+    'stored_event_repository' => Spatie\EventSourcing\StoredEvents\Repositories\EloquentStoredEventRepository::class,
 
     /*
      * This class is responsible for storing snapshots. To add extra behaviour you
      * can change this to a class of your own. The only restriction is that
-     * it should implement \Spatie\EventSourcing\EloquentSnapshotRepository.
+     * it should implement \Spatie\EventSourcing\Snapshots\EloquentSnapshotRepository.
      */
-    'snapshot_repository' => \Spatie\EventSourcing\Snapshots\EloquentSnapshotRepository::class,
+    'snapshot_repository' => Spatie\EventSourcing\Snapshots\EloquentSnapshotRepository::class,
+
+    /*
+     * This class is responsible for storing events in the EloquentSnapshotRepository.
+     * To add extra behaviour you can change this to a class of your own. It should
+     * extend the \Spatie\EventSourcing\Snapshots\EloquentSnapshot model.
+     */
+    'snapshot_model' => Spatie\EventSourcing\Snapshots\EloquentSnapshot::class,
 
     /*
      * This class is responsible for handling stored events. To add extra behaviour you
      * can change this to a class of your own. The only restriction is that
-     * it should implement \Spatie\EventSourcing\HandleDomainEventJob.
+     * it should implement \Spatie\EventSourcing\StoredEvents\HandleDomainEventJob.
      */
-    'stored_event_job' => \Spatie\EventSourcing\StoredEvents\HandleStoredEventJob::class,
+    'stored_event_job' => Spatie\EventSourcing\StoredEvents\HandleStoredEventJob::class,
 
     /*
      * Similar to Relation::morphMap() you can define which alias responds to which
@@ -81,21 +94,36 @@ return [
      * and stored as json. You can customize the class name. A valid serializer
      * should implement Spatie\EventSourcing\EventSerializers\EventSerializer.
      */
-    'event_serializer' => \Spatie\EventSourcing\EventSerializers\JsonEventSerializer::class,
+    'event_serializer' => Spatie\EventSourcing\EventSerializers\JsonEventSerializer::class,
 
     /*
-     * When replaying events, potentially a lot of events will have to be retrieved.
-     * In order to avoid memory problems events will be retrieved as chunks.
-     * You can specify the chunk size here.
+     * These classes normalize and restore your events when they're serialized. They allow
+     * you to efficiently store PHP objects like Carbon instances, Eloquent models, and
+     * Collections. If you need to store other complex data, you can add your own normalizers
+     * to the chain. See https://symfony.com/doc/current/components/serializer.html#normalizers
      */
-    'replay_chunk_size' => 1000,
+    'event_normalizers' => [
+        Spatie\EventSourcing\Support\CarbonNormalizer::class,
+        Spatie\EventSourcing\Support\ModelIdentifierNormalizer::class,
+        Symfony\Component\Serializer\Normalizer\DateTimeNormalizer::class,
+        Symfony\Component\Serializer\Normalizer\ArrayDenormalizer::class,
+        Spatie\EventSourcing\Support\ObjectNormalizer::class,
+    ],
 
     /*
      * In production, you likely don't want the package to auto-discover the event handlers
      * on every request. The package can cache all registered event handlers.
-     * More info: https://docs.spatie.be/laravel-event-sourcing/v1/advanced-usage/discovering-projectors-and-reactors
+     * More info:
+     * https://spatie.be/docs/laravel-event-sourcing/v4/advanced-usage/discovering-projectors-and-reactors#caching-discovered-projectors-and-reactors
      *
      * Here you can specify where the cache should be stored.
      */
     'cache_path' => storage_path('app/event-sourcing'),
+
+    /*
+     * When storable events are fired from aggregates roots, the package can fire off these
+     * events as regular events as well.
+     */
+
+    'dispatch_events_from_aggregate_roots' => false,
 ];

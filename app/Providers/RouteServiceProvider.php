79f65c1d556d373589bbb637cb35624f633afee7
Shaun Keating <shaun@blackfrog.co>
2023-04-29 14:55:11 +0100

    Fix missing HOME const in RouteServiceProvider.php and composer update.

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
 {
+    public const HOME = '/home';
+
     /**
      * Define your route model bindings, pattern filters, etc.
      *
